aa623c316cb77ace020ead7c75b6c232bafd82e0
Polina Radahuz <polina.radahuz@gmail.com>
2023-03-01 23:46:17 +0100

    added first MUI components

--- src/App.js
+++ src/App.js
@@ -3,18 +3,30 @@ import './App.css';
 import '@fontsource/roboto/300.css';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import SendIcon from '@mui/icons-material/Send';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-     
-        <p>
-          hell yeah
-        </p>
-        <p>
-          let's try and make some online chat layout
-        </p>
+<Box 
+  component="form"
+  sx={{
+    '& .MuiTextField-root': { m: 1, width: '25ch' },
+  }}
+  noValidate
+  autoComplete="off">
+        <TextField 
+            id="outlined-textarea"
+            label="Enter your message"
+            placeholder="Enter your message"
+            multiline
+        />
+     <Button variant="text"><SendIcon/></Button>
+        </Box>
+       
       </header>
      
       </div>
