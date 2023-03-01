a4f265ce81af2392dbb62e28ac29204b4c11d0a0
Polina Radahuz <polina.radahuz@gmail.com>
2023-03-01 23:22:15 +0100

    first check

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import '@fontsource/roboto/300.css';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+     
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          hell yeah
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          let's try and make some online chat layout
+        </p>
       </header>
-    </div>
-  );
+     
+      </div>
+);
 }
 
 export default App;
